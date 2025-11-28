822454e5b9d654df0dfa74e309feb55104ccaa3e
2002goblue <cliomustang2002@gmail.com>
2025-11-27 21:20:29 -0500

    Track and log post-init average FPS
    Adds tracking for frames and average FPS during the 10 seconds after initialization. Logs the average FPS for this period and refactors init end time handling for more accurate duration measurement.

--- com/mojang/minecraft/Minecraft.java
+++ com/mojang/minecraft/Minecraft.java
@@ -69,6 +69,9 @@ public class Minecraft implements Runnable {
    private long initFrames = 0;
    private float avgFps;
    private String avgFpsStatus = "in progress";
+   private long postInitFrames = 0;
+   private float avgPostInitFps;
+   private long initEndTime;
 
 
    public Minecraft(Canvas parent, int width, int height, boolean fullscreen) {
@@ -477,6 +480,14 @@ public class Minecraft implements Runnable {
    public void render(float a) {
       if (!initDone) {
         initFrames++;
+      } else {
+         if(System.currentTimeMillis() - initEndTime > 10000) {
+            avgPostInitFps = postInitFrames / ((System.currentTimeMillis() - initEndTime)/1000);
+            initEndTime = System.currentTimeMillis();
+            System.out.println("Avg FPS During 10s After Init: " + Float.toString(avgPostInitFps));
+         } else {
+            postInitFrames++;
+         }
       }
       if (!Display.isActive()) {
          this.releaseMouse();
@@ -517,7 +528,8 @@ public class Minecraft implements Runnable {
 
         if (dirtyChunkCount == 0) {    
             initDone = true;
-            initDuration = System.currentTimeMillis() - this.startupTime;
+            initEndTime = System.currentTimeMillis();
+            initDuration = initEndTime - this.startupTime;
             initDurationStatus = Long.toString(initDuration);
 
             if (initDuration > 0) {
